6f39da9c3bd29f6b499bd3be954196889fe4458a
Sam Wang <sevenbamboos@gmail.com>
2017-03-10 19:51:55 +0800

    add basic route & css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PipelineModule } from './pipeline/pipeline.module';
+import { SearchScreenComponent as PipelineSearchScreen } from './pipeline/search-screen/search-screen.component';
+import { DetailScreenComponent as PipelineDetailScreen } from './pipeline/detail-screen/detail-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    PipelineModule
+    PipelineModule,
+    RouterModule.forRoot([
+      {
+        path: 'pipeline/list',
+        component: PipelineSearchScreen 
+      },
+      {
+        path: 'pipeline/detail',
+        component: PipelineDetailScreen 
+      }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
